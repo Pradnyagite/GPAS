0ca285288b3329cb77b0d85d9de7f9043725b65d
Pradnya Gite <pradnyagite98@gmail.com>
2020-01-31 13:14:00 +0530

    abc

--- app/src/main/java/com/NRB/gpas/LoginActivity.java
+++ app/src/main/java/com/NRB/gpas/LoginActivity.java
@@ -1,14 +1,20 @@
 package com.NRB.gpas;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.support.annotation.NonNull;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -16,6 +22,8 @@ public class LoginActivity extends AppCompatActivity {
     EditText username, password;
     Button login;
     String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    private FirebaseAuth mAuth;
+    private ProgressDialog mProLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +60,7 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         }
+        signin(user, pass);
 
         Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
         loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -63,6 +72,36 @@ public class LoginActivity extends AppCompatActivity {
     public void forgotPass(View view) {
         Intent intent = new Intent(this, ForgotPassword.class);
         startActivity(intent);
+    }
+
+    private void signin(final String user, String pass) {
+
+        final String tempuser = user, temppassword = pass;
+        mAuth.signInWithEmailAndPassword(user, pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+
+                    mProLogin.dismiss();
+                    if (FirebaseAuth.getInstance().getCurrentUser().getUid().equals("hgFkE0JFEsbiAF8xxIlsRaBrqr63")) {
+
+                        Intent loginIntent = new Intent(LoginActivity.this, UserMapsActivity.class);
+                        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(loginIntent);
+                    } else {
+
+                        Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
+                        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(loginIntent);
+                    }
+
+                } else {
+                    mProLogin.dismiss();
+                    Toast.makeText(getApplication(), "Username Or Password Incorrect", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 
 }
