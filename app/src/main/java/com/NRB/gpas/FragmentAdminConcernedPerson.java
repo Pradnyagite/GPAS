68be755592950830b40378dc5e9f733bdee7b2a3
aniketK98 <36024733+aniketK98@users.noreply.github.com>
2020-03-13 11:37:51 +0530

    locatio

--- app/src/main/java/com/NRB/gpas/FragmentAdminConcernedPerson.java
+++ app/src/main/java/com/NRB/gpas/FragmentAdminConcernedPerson.java
@@ -4,103 +4,115 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link FragmentAdminConcernedPerson.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link FragmentAdminConcernedPerson#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FragmentAdminConcernedPerson extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
+    private static final String URL_VISITORS = "http://192.168.29.229/android/scrpit.php";
+    List<VisitorInfo> visitorInfoList;
+    RecyclerView recyclerView;
+    private Spinner spinner1;
     public FragmentAdminConcernedPerson() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentAdminConcernedPerson.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentAdminConcernedPerson newInstance(String param1, String param2) {
-        FragmentAdminConcernedPerson fragment = new FragmentAdminConcernedPerson();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_concerned_person, container, false);
+        View v=inflater.inflate(R.layout.fragment_admin_concerned_person, container, false);
+        recyclerView = v.findViewById(R.id.recyclerView5);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        spinner1 = v.findViewById(R.id.spinner1);
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                loadVisitors();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        return v;
+    }
+    private void loadVisitors() {
+        visitorInfoList = new ArrayList<>();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL_VISITORS,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            //converting the string to json array object
+                            JSONArray array = new JSONArray(response);
+
+                            //traversing through all the object
+                            for (int i = 0; i < array.length(); i++) {
+
+                                //getting product object from json array
+                                JSONObject visitor = array.getJSONObject(i);
+
+                                String conernP = String.valueOf(spinner1.getSelectedItem());
+                                if (visitor.getString("conernP").equals(conernP)) {
+                                    //adding the product to product list
+                                    visitorInfoList.add(new VisitorInfo(
+                                            visitor.getInt("id"),
+                                            visitor.getString("name"),
+                                            visitor.getString("address"),
+                                            visitor.getString("email"),
+                                            visitor.getString("contact"),
+                                            visitor.getString("gender"),
+                                            visitor.getString("org"),
+                                            visitor.getString("vD"),
+                                            visitor.getString("vT"),
+                                            visitor.getString("leaveT"),
+                                            visitor.getString("conernP"),
+                                            visitor.getString("purpose"),
+                                            visitor.getString("status")
+                                    ));
+                                }
+                            }
+                            //creating adapter object and setting it to recyclerview
+                            VisitorAdaptor adapter = new VisitorAdaptor(visitorInfoList);
+                            recyclerView.setAdapter(adapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+
+        //adding our stringrequest to queue
+        Volley.newRequestQueue(getActivity()).add(stringRequest);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
