b4019931bb881785a239e9e18524395772d66c2f
aniketK98 <36024733+aniketK98@users.noreply.github.com>
2020-03-29 02:44:04 +0530

    Login

--- app/src/main/java/com/NRB/gpas/IPString.java
+++ app/src/main/java/com/NRB/gpas/IPString.java
@@ -2,4 +2,5 @@ package com.NRB.gpas;
 
 public class IPString {
     public static String ip = "http://192.168.29.229/android/scrpit.php";
+    public static String loginString = "http://192.168.29.229/android/login.php";
 }
