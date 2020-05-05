ebad4f7609b3f6dd466489f39d23cdaa12ce45f7
aniketK98 <36024733+aniketK98@users.noreply.github.com>
2020-05-05 15:43:21 +0530

    khup UI drawable

--- app/src/main/java/com/NRB/gpas/IPString.java
+++ app/src/main/java/com/NRB/gpas/IPString.java
@@ -1,12 +1,20 @@
 package com.NRB.gpas;
 
 public class IPString {
-    public static String ip = "http://192.168.29.229/BEProject/Project/android/scrpit.php";
-    public static String loginString = "http://192.168.29.229/BEProject/Project/android/login.php";
-    public static String loadPersonString = "http://192.168.29.229/BEProject/Project/android/getUser.php";
-    public static String UrlInsert = "http://192.168.29.229/BEProject/Project/android/adduser.php";
-    public static String UrlPass = "http://192.168.29.229/BEProject/Project/android/changePass.php";
-    public static String UrlButtonScripts = "http://192.168.29.229/BEProject/Project/android/buttonscripts.php";
-    public static String UrlUsers = "http://192.168.29.229/BEProject/Project/android/loadUsers.php";
+//    public static String ip = "http://192.168.29.229/BEProject/Project/android/scrpit.php";
+//    public static String loginString = "http://192.168.29.229/BEProject/Project/android/login.php";
+//    public static String loadPersonString = "http://192.168.29.229/BEProject/Project/android/getUser.php";
+//    public static String UrlInsert = "http://192.168.29.229/BEProject/Project/android/adduser.php";
+//    public static String UrlPass = "http://192.168.29.229/BEProject/Project/android/changePass.php";
+//    public static String UrlButtonScripts = "http://192.168.29.229/BEProject/Project/android/buttonscripts.php";
+//    public static String UrlUsers = "http://192.168.29.229/BEProject/Project/android/loadUsers.php";
+
+    public static String ip = "https://gatepassauto.000webhostapp.com/BEProject/Project/android/scrpit.php";
+    public static String loginString = "https://gatepassauto.000webhostapp.com/BEProject/Project/android/login.php";
+    public static String loadPersonString = "https://gatepassauto.000webhostapp.com/BEProject/Project/android/getUser.php";
+    public static String UrlInsert = "https://gatepassauto.000webhostapp.com/BEProject/Project/android/adduser.php";
+    public static String UrlPass = "https://gatepassauto.000webhostapp.com/BEProject/Project/android/changePass.php";
+    public static String UrlButtonScripts = "https://gatepassauto.000webhostapp.com/BEProject/Project/android/buttonscripts.php";
+    public static String UrlUsers = "https://gatepassauto.000webhostapp.com/BEProject/Project/android/loadUsers.php";
 
 }
